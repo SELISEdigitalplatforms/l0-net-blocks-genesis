4b4a12a9086af0d9bbae63f0df1db2ddb6cfcbff
Koushik Ghosh <koushik.ghosh@selise.ch>
2024-10-24 18:33:12 +0600

    updateTokenParameters

--- src/Genesis/Auth/JwtTokenParameters.cs
+++ src/Genesis/Auth/JwtTokenParameters.cs
@@ -6,6 +6,12 @@
         public List<string> Audiences { get; init; }
         public string SigningKeyPath { get; init; }
         public string SigningKeyPassword { get; init; }
+        public string PrivateCertificatePassword { get; init; }
+        public string PublicCertificatePassword { get; init; }
+        public string Subject { get; init; }
+        public int ValidForNumberOfDays { get; init; }
+        public DateTime IssueDate { get; init; }
+
     }
 
 }
