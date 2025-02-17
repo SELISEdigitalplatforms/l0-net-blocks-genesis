8b751c66d21f3ebf44c8aff7b57fe3fbac57fbe8
Koushik Ghosh <koushik.ghosh@selise.ch>
2025-02-17 12:44:13 +0600

    updated

--- src/Genesis/Tenant/Tenant.cs
+++ src/Genesis/Tenant/Tenant.cs
@@ -20,7 +20,7 @@ namespace Blocks.Genesis
         public required JwtTokenParameters JwtTokenParameters { get; set; }
         public List<string> AllowedGrantType { get; set; } = new List<string>();
         public int AccessTokenValidForNumberMinutes { get; init; } = 7;
-        public int RefreshTokenValidForNumberMinutes { get; init; } = 30;
+        public int RefreshTokenValidForNumberMinutes { get; set; } = 30;
         public int RememberMeRefreshTokenValidForNumberMinutes { get; init; } = 30 * 60 * 24;
         public int GetNumberOfWrongAttemptsToLockTheAccount { get; set; } = 5;
         public int AccountLockDurationInMinutes { get; set; } = 5;
