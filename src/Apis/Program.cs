fb6daa6739cbf3e4afd2e34cd25b75b07222a68a
Koushik Ghosh <koushik.ghosh@selise.ch>
2025-04-15 19:15:09 +0600

    migrated into dotnet 9

--- src/Apis/Program.cs
+++ src/Apis/Program.cs
@@ -1,16 +1,11 @@
 using Blocks.Genesis;
 using GrpcServiceTestTemp.Services;
 
-
-
 const string _serviceName = "Service-API-Test_Two";
-
 var blocksSecret = await ApplicationConfigurations.ConfigureLogAndSecretsAsync(_serviceName);
-
 var builder = WebApplication.CreateBuilder(args);
-
 ApplicationConfigurations.ConfigureApiEnv(builder, args);
-ApplicationConfigurations.ConfigureKestrel(builder);
+//ApplicationConfigurations.ConfigureKestrel(builder);
 
 var services = builder.Services;
 
@@ -21,14 +16,13 @@ ApplicationConfigurations.ConfigureServices(services, new MessageConfiguration
     Topics = new List<string> { "demo_topic_1" },
     ServiceName = _serviceName,
 });
+
 ApplicationConfigurations.ConfigureApi(services);
-
 var app = builder.Build();
-
 ApplicationConfigurations.ConfigureMiddleware(app);
 
 app.MapGrpcService<GreeterService>();
 
-app.Run();
+await app.RunAsync();
 
 
